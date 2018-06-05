0ac98a3017ca4071ca6451969a52787d43692172
Megan Collier <meganraec@live.com>
2018-06-05 03:24:51 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -55,59 +55,8 @@ class Owner
   def feed_fish
     Fish.ALL.each {|fish| fish.mood = "happy"}
   end
-end
 
-    #
-    # describe "#walk_dogs" do
-    #   it "walks the dogs which makes the dogs' moods happy" do
-    #     dog = Dog.new("Daisy")
-    #     owner.pets[:dogs] << dog
-    #     owner.walk_dogs
-    #     expect(dog.mood).to eq("happy")
-    #   end
-    # end
-    #
-    # describe "#play_with_cats" do
-    #   it "plays with the cats which makes the cats moods happy" do
-    #     cat = Cat.new("Muffin")
-    #     owner.pets[:cats] << cat
-    #     owner.play_with_cats
-    #     expect(cat.mood).to eq("happy")
-    #   end
-    # end
-    #
-    # describe "#feed_fish" do
-    #   it "feeds the fishes which makes the fishes' moods happy" do
-    #     fish = Fish.new("Nemo")
-    #     owner.pets[:fishes] << fish
-    #     owner.feed_fish
-    #     expect(fish.mood).to eq("happy")
-    #   end
-    # end
-    #
-    # describe "#sell_pets" do
-    #   it 'can sell all its pets, which make them nervous' do
-    #     fido = Dog.new("Fido")
-    #     tabby = Cat.new("Tabby")
-    #     nemo = Fish.new("Nemo")
-    #     [fido, tabby, nemo].each {|o| o.mood = "happy" }
-    #     owner.pets = {
-    #       :dogs => [fido, Dog.new("Daisy")],
-    #       :fishes => [nemo],
-    #       :cats => [Cat.new("Mittens"), tabby]
-    #     }
-    #     owner.sell_pets
-    #     owner.pets.each {|type, pets| expect(pets.empty?).to eq(true) }
-    #     [fido, tabby, nemo].each { |o| expect(o.mood).to eq("nervous") }
-    #   end
-    # end
-    #
-    # describe "#list_pets" do
-    #   it 'can list off its pets' do
-    #     owner.buy_fish("Bubbles")
-    #     owner.buy_fish("Nemo")
-    #     owner.buy_cat("Crookshanks")
-    #     owner.buy_dog("Fido")
-    #     owner.buy_dog("Snuffles")
-    #     owner.buy_dog("Charley")
-    #     expect(owner.list_pets).to eq("I have 2 fish, 3 dog(s), and 1 cat(s).")
+  def list_pets
+    self.pets
+  end
+end
