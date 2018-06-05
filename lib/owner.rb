8027841bc09a891be95693d4f40943cae70648b9
Megan Collier <meganraec@live.com>
2018-06-05 03:35:11 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -56,6 +56,13 @@ class Owner
     self.pets[:fishes].each {|fish| fish.mood = "happy"}
   end
 
+  def sell_pets
+    self.pets.each do |k, v|
+      v.each do |pet| pet.mood = "nervous"
+      end
+    end
+  end
+
   def list_pets
     "I have #{self.pets[:fishes].count} fish, #{self.pets[:dogs].count} dog(s), and #{self.pets[:cats].count} cat(s)."
   end
