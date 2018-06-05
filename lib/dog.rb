0dffe801408c8bdf6a1f45d0521ef48592674c53
Megan Collier <meganraec@live.com>
2018-06-05 03:19:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood
+
+  @@dogs = []
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+    @@dogs << self
+  end
 end
